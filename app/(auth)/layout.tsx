354351b0f806be43f99c3105a908695742679e69
Sonny-Cao <sonnycao@ufl.edu>
2026-01-16 16:17:43 -0500

    Auth fixed
    Middleware fixed, could not migrate to proxy; moving forward

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function AuthLayout({children}: {children: React.ReactNode}) {
   return (
-    <div className='bg-red-500 h-full'>
+    <div className='h-full flex item-center justify-center'>
       {children}
     </div>
   )
