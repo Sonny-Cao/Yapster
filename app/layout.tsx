6a884aa9a14dc8a89c7258bc32dd2bfb40c9c624
Sonny-Cao <sonnycao@ufl.edu>
2026-01-16 15:05:56 -0500

    auth setup incomplete
    middleware deprecated, migrate to proxy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Open_Sans } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const font = Open_Sans({subsets: ['latin']})
 const geistSans = Geist({
@@ -24,12 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={font.className}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={font.className}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
