38f305f54910c9a2a98bf593ba0bf1a542f39322
Sonny-Cao <sonnycao@ufl.edu>
2026-01-16 20:02:08 -0500

    prisma client
    added prisma adapter and finetuning for new vs existing users

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,16 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg';
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+export const db = globalThis.prisma || prisma;
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma= db;
