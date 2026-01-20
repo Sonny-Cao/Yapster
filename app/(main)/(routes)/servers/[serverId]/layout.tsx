de7528cefc43953e837437da38bcdff59b1c34ed
Sonny-Cao <sonnycao@ufl.edu>
2026-01-19 20:11:28 -0500

    Server Sidebar
    switching servers display server name on top of sidebar;

---
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -0,0 +1,57 @@
+import { ServerSidebar } from "@/components/server/server-sidebar";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+
+const ServerIdLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { serverId: string };
+}) => {
+
+  const { serverId } = await params;
+  console.log("=== SERVER ID LAYOUT MOUNT ===");
+  console.log("params object:", params);
+  console.log("params.serverId:", params.serverId);
+
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return <RedirectToSignIn/>;
+  }
+  
+  const server = await db.server.findFirst({
+  where: {
+    id: params.serverId,
+    members: {
+      some: {
+        profileId: profile.id,
+      },
+    },
+  },
+  });
+
+  if (!server) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="h-full flex">
+      <div 
+        className="max-sm:hidden md:flex h-full w-60 z-20 flex-col 
+        fixed inset-y-0">
+        <ServerSidebar 
+        serverId={serverId}/>
+      </div>
+      <main className="h-full md:pl-60">
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default ServerIdLayout;
